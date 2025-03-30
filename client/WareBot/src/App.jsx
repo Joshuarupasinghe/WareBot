eca21acd5261ef79a0563e83bac5e58e04ca6c7d
VihangaDewindi <137777722+VihangaDewindi@users.noreply.github.com>
2025-03-31 02:00:23 +0530

    Frontend
    Added Inventory Management - Soon to be Expired Stocks
    Added Alerts & Notifications - Expiration Alert

--- client/WareBot/src/App.jsx
+++ client/WareBot/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Layout from './components/Layout';
+import InventoryManagement from './components/InventoryManagement';
+import ExpirationAlert from './components/ExpirationAlert';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<Dashboard />} />
+        <Route path="/inventory-management" element={<InventoryManagement />} />
+        <Route path='/expirationalert' element={<ExpirationAlert />} />
       </Routes>
     </Layout>
   );
