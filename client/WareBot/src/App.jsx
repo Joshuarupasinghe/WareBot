34a31e9bf643da3d77ee434f9d2f0eabe8d939b5
agasthi23 <137294353+agasthi23@users.noreply.github.com>
2025-04-07 23:50:57 +0530

    frontend - Qr generating page
    developed the frontend of the Qr genrating page

--- client/WareBot/src/App.jsx
+++ client/WareBot/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Layout from './components/Layout';
+import QRPage from './pages/QrGenerator';
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<Dashboard />} />
+        <Route path="/Qrpage"element={<QRPage/>}/>
       </Routes>
     </Layout>
   );
