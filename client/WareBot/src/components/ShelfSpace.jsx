f8a4fc7d9c3b07876fea677dccca810e764a1069
Kavishka Sulochana <kavishkaigalagama@gmail.com>
2025-04-08 16:30:27 +0530

    Dashboard Layout

---
+++ client/WareBot/src/components/ShelfSpace.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShelfSpace = () => {
+  return (
+    <div>
+        <div className='bg-amber-600 h-30 w-full'>Shelf Space</div>
+    </div>
+  )
+}
+
+export default ShelfSpace
---
+++ client/WareBot/src/components/StockLevelOverview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StockLevelOverview = () => {
+  return (
+    <div>
+        <div className='bg-amber-600 h-30 w-full'>StockLevelOverview</div>
+    </div>
+  )
+}
+
+export default StockLevelOverview
