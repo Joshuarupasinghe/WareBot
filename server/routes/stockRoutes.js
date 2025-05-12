0aa294045990725018716ea68f0b6f760c648921
VihangaDewindi <137777722+VihangaDewindi@users.noreply.github.com>
2025-05-12 22:23:38 +0530

    Updated stockRoutes
    Added expiring and id endpoint

--- server/routes/stockRoutes.js
+++ server/routes/stockRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addStock,getStockIdCounter,getExpiringStocks, } = require("../controllers/stockController");
+const { addStock,getStockIdCounter,getExpiringStocks, getStockById } = require("../controllers/stockController");
 const router = express.Router();
 
 // Add stock endpoint
@@ -10,5 +10,9 @@ router.get("/get-counter", getStockIdCounter);
 
 // Get expiring stocks
 router.get("/expiring", getExpiringStocks);
+
+// Get stock details by StockId
+router.get("/:id", getStockById);
+
 
 module.exports = router;
